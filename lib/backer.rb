3c41505ef53a4044fc854dbf0031a0d5ac653b7a
Andrew Bernier <a6bernier@gmail.com>
2018-10-19 16:27:32 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,8 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(Project)
-    @backed_projects << Project
+  def back_project(Project.title)
+    @backed_projects << Project.title
   end
 
 
